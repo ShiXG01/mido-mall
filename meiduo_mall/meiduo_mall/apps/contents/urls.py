df030895337c80596b941e51d3e359ad9eabbb08
liang <1843440138@qq.com>
2023-03-13 15:59:58 +0800

    添加一个首页的路由

--- meiduo_mall/meiduo_mall/apps/contents/urls.py
+++ meiduo_mall/meiduo_mall/apps/contents/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexViews.as_view(), name='index'),
-    url(r'^index/$', views.IndexViews.as_view(), name='index1'),
+    url(r'^index/$', views.IndexViews.as_view()),
 ]
