efdc36df950576b1a1e393a0a6fffe3e0f606a2c
liang <1843440138@qq.com>
2023-03-28 20:52:33 +0800

    定义分页器

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/utils.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/utils.py
@@ -1,3 +1,7 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
 # 重写JWT返回结果方法
 def jwt_response_payload_handler(token, user=None, request=None):
     return {
@@ -5,3 +9,18 @@ def jwt_response_payload_handler(token, user=None, request=None):
         'username': user.username,
         'id': user.id,
     }
+
+
+# 自定义分页器
+class PageNum(PageNumberPagination):
+    page_size_query_param = 'pagesize'
+    max_page_size = 10
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'list': data,
+            'page': self.page.number,
+            'pages': self.page.paginator.num_pages,
+            'pagesize': self.max_page_size
+        })
